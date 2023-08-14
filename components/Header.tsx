edc753f5f47c39a72540ea7ef02de714c6a93955
Rene Groothuis <renegrooth@gmail.com>
2023-08-14 16:46:37 +1200

    NavBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Logout from './Logout'
 
 function Header() {
   const session = true
+
+  if (session)
+    return (
+      <header className="sticky top-0 z-50 bg-white shadow-sm flex justify-between items-center p-10 ">
+        <div className="flex space-x-2">
+          <Image
+            src={'/'}
+            alt="user image"
+            className="rounded-full mx-2 object-contain"
+            height={10}
+            width={50}
+          />
+          <div>
+            <p className="text-purple-400">Logged in as:</p>
+            <p className="text-lg font-bold">Rene Groothuis</p>
+          </div>
+        </div>
+
+        <Logout />
+      </header>
+    )
 
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm flex justify-center items-center p-10 ">
---
+++ components/Logout.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+function Logout() {
+  return (
+    <div>
+      <button
+        onClick={() => {}}
+        className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Sign out
+      </button>
+    </div>
+  )
+}
+export default Logout
